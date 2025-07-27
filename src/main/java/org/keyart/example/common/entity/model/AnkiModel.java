54896eba6c4f5f21858c8f372233ced3b3b23b88
KeyartDev <keyart2108@gmail.com>
2025-07-27 16:30:01 +0300

    Добавлено: - Сущность(При использования GeckoLib); - Класс-помощник ExecuteDelayed, который позволит выполнять блок кода с задержкой.

--- src/main/java/org/keyart/example/common/entity/model/AnkiModel.java
+++ src/main/java/org/keyart/example/common/entity/model/AnkiModel.java
@@ -1,9 +1,14 @@
 package org.keyart.example.common.entity.model;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import org.keyart.example.Example;
 import org.keyart.example.common.entity.AnkiEntity;
+import software.bernie.geckolib.constant.DataTickets;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
+import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
+import software.bernie.geckolib.model.data.EntityModelData;
 
 public class AnkiModel extends GeoModel<AnkiEntity> {
     public static final ResourceLocation ANKI_MODEL = ResourceLocation.fromNamespaceAndPath(Example.MODID, "geo/anki.geo.json");
@@ -23,5 +28,17 @@ public class AnkiModel extends GeoModel<AnkiEntity> {
     @Override
     public ResourceLocation getAnimationResource(AnkiEntity ankiEntity) {
         return ANKI_ANIMATIONS;
+    }
+
+    @Override
+    public void setCustomAnimations(AnkiEntity animatable, long instanceId, AnimationState<AnkiEntity> animationState) {
+        CoreGeoBone head = getAnimationProcessor().getBone("head");
+
+        if (head != null) {
+            EntityModelData entityData = animationState.getData(DataTickets.ENTITY_MODEL_DATA);
+
+            head.setRotX((360 - entityData.headPitch()) * Mth.DEG_TO_RAD);
+            head.setRotY(entityData.netHeadYaw() * Mth.DEG_TO_RAD);
+        }
     }
 }
