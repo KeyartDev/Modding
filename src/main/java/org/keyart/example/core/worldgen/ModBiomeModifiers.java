c10498880050ccc526d680497c82a32b9c87f188
KeyartDev <keyart2108@gmail.com>
2025-11-22 15:02:54 +0300

    Добавлено: - Генерация дерева в мире. Изменено: --

--- src/main/java/org/keyart/example/core/worldgen/ModBiomeModifiers.java
+++ src/main/java/org/keyart/example/core/worldgen/ModBiomeModifiers.java
@@ -7,6 +7,7 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,6 +15,8 @@ import org.keyart.example.Example;
 
 public class ModBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_SOME_ORE = registerKey("add_some_ore");
+
+    public static final ResourceKey<BiomeModifier> ADD_TREE_VERUS = registerKey("add_tree_verus");
 
 
     public static void bootstrap(BootstapContext<BiomeModifier> context) {
@@ -25,6 +28,11 @@ public class ModBiomeModifiers {
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.SOME_ORE_PLACED_KEY)),
                 GenerationStep.Decoration.UNDERGROUND_ORES));
 
+        context.register(ADD_TREE_VERUS, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(Tags.Biomes.IS_PLAINS),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.VERUS_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION
+        ));
     }
 
     private static ResourceKey<BiomeModifier> registerKey(String name) {
--- src/main/java/org/keyart/example/core/worldgen/ModPlacedFeatures.java
+++ src/main/java/org/keyart/example/core/worldgen/ModPlacedFeatures.java
@@ -4,6 +4,8 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -12,11 +14,14 @@ import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 import org.keyart.example.Example;
+import org.keyart.example.core.registry.ModBlocks;
 
 import java.util.List;
 
 public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> SOME_ORE_PLACED_KEY = registerKey("some_ore_placed");
+
+    public static final ResourceKey<PlacedFeature> VERUS_PLACED_KEY = registerKey("verus_placed");
 
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
@@ -24,6 +29,10 @@ public class ModPlacedFeatures {
         register(context, SOME_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_SOME_ORE_KEY),
                 ModOrePlacement.commonOrePlacement(12,
                         HeightRangePlacement.uniform(VerticalAnchor.absolute(0), VerticalAnchor.absolute(80))));
+
+        register(context, VERUS_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.VERUS_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2),
+                        ModBlocks.VERUS_SAPLING.get()));
     }
 
 
